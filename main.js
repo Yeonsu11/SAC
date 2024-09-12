b6b9f5d2ec7fbc15fd2e7907fa77f78f7d431034
Yeonsu11 <yeonsu.kim@jadedata.net>
2024-09-12 13:28:25 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,22 @@
  this._shadowRoot.appendChild(template.content.cloneNode(true))
  this._root = this._shadowRoot.getElementById('root')
  }
+ 
+ onCustomWidgetResize (width, height) {
+	 this.render()
  }
+ 
+ onCustomWidgetAfterUpdate(changeProps){
+ }
+ 
+ onCustomWidgetDestroy() {
+ }
+ 
+ render() {
+	 this.root.textContent = 'Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight}'
+ }
+ }
+ 
  customElements.define('com-sap-sac-exercise-yskim-main', Main)
+ 
  })()
